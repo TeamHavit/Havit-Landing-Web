c723f8ee7a0de43843e8b1c94d4fb3f1c4537906
jokj624 <jokj624@gmail.com>
2023-05-07 04:43:56 +0900

    Feat: 랜딩 기본 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import '../../public/static/fonts/style.css';
+
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
