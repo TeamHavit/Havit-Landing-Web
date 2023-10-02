59717382d661b271f4adc5f06ab889bee39ed3ac
jokj624 <jokj624@gmail.com>
2023-10-02 16:15:50 +0900

    feat(app): next-seo 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,15 @@ import '@/styles/globals.scss';
 import '../../public/static/fonts/style.css';
 
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from '@/constants/objects/seo';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
